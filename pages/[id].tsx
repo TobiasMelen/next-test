6a6e47b709046160f832e872adc69e825eb02218
Tobbe <tobbe@Tobbes-MacBook-Air.local>
2021-10-22 09:02:10 +0200

    Try server side props

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,18 +1,17 @@
 import { GetStaticPaths, GetStaticProps } from "next";
-import { CSSProperties } from "react";
 import Hello from "../src/components/Hello";
 
 export default function Home(
-  props: ReturnType<typeof getStaticProps>["props"]
+  props: ReturnType<typeof getServerSideProps>["props"]
 ) {
   return <Hello {...props} />;
 }
 
-export const getStaticProps = ({ params: { id } }) => ({
+export const getServerSideProps = ({ params: { id } }) => ({
   props: { id },
 });
 
-export const getStaticPaths: GetStaticPaths = async () => ({
-  paths: [{ params: { id: "world" } }],
-  fallback: "blocking",
-});
+//export const getStaticPaths: GetStaticPaths = async () => ({
+//   paths: [{ params: { id: "world" } }],
+//   fallback: "blocking",
+// });
