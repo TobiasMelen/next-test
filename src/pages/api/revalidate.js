95a7aaa0fbdb7476bb5451681d8275f9b76a4782
Tobbe <tobbe@Tobbes-MacBook-Air.local>
2022-02-19 12:40:32 +0100

    Unstable revalidate update

--- src/pages/api/revalidate.js
+++
@@ -1,4 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default async function revalidate(req, res) {
-  await res.unstable_revalidate("*");
-}
---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,20 @@
+import { NextApiHandler } from "next";
+import { createContentfulClient, ContentfulArticle } from "../../contentful";
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const revalidate: NextApiHandler = async (_, res) => {
+  const contentfulClient = createContentfulClient();
+  await Promise.all(
+    [
+      "/",
+      ...(
+        await contentfulClient.getEntries<ContentfulArticle>({
+          content_type: "article",
+        })
+      ).items.map((s) => s.fields.slug),
+    ].map(res.unstable_revalidate.bind(res))
+  );
+};
+
+export default revalidate;
