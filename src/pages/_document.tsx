64b15ab0dc3065c46a7d34250df510bdb399ab60
Tobbe <tobbe@Tobbes-MacBook-Air.local>
2022-02-27 09:47:15 +0100

    New document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,36 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
 import { extractCss } from "goober";
 
-export default function Document({ css } = { css: "" }) {
-  <Html>
-    <Head>
-      <style
-        id={"_goober"}
-        // And defined it in here
-        dangerouslySetInnerHTML={{ __html: " " + css }}
-      />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>;
-}
+export default class MyDocument extends Document<{ css: string }> {
+  static async getInitialProps({ renderPage }: DocumentContext) {
+    const page = renderPage();
 
-export function getInitialProps({ renderPage }: any) {
-  const page = renderPage();
-  const css = extractCss(page);
-  return { ...page, css };
+    // Extrach the css for each page render
+    const css = extractCss();
+    return { ...page, css };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style
+            id={"_goober"}
+            // And defined it in here
+            dangerouslySetInnerHTML={{ __html: " " + this.props.css }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
